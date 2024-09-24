eab885b4f6806cc6aa7a8bec0f3575287f660363
fred4impact <runtesting4me@gmail.com>
2024-09-24 23:58:26 +0100

    update some images and otehrs

--- course_platform/settings.py
+++ course_platform/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -146,9 +147,6 @@ JAZZMIN_SETTINGS = {
     # "order_with_respect_to": ["core", 'userauths', "transactions" , 'addon', 'blog']
  
 }
-
-
-
 
 
 
